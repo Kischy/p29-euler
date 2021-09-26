d036c9050c6227f221056627ad9208a85e12d214
Christian Kischy Haege <kischy@gmx.de>
2021-09-26 20:00:47 +0200

    Added inital files

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Calculation started");
+
+    let answer_p29: u128 = 0;
+
+    println!(
+        "The answer to problem 29 of project Euler is {}.",
+        answer_p29
+    );
+}
